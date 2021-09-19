6dffc06ffb663dfde6adbb4d571e2d30a836cf12
Yangshun <tay.yang.shun@gmail.com>
2021-09-20 00:27:40 +0800

    feat: add Jest

---
+++ packages/add/__tests__/add.test.js
@@ -0,0 +1,5 @@
+import add from '@yangshun/add';
+
+test('add', () => {
+  expect(add(1, 2)).toBe(3);
+});
---
+++ packages/subtract/__tests__/subtract.test.js
@@ -0,0 +1,5 @@
+import subtract from '@yangshun/subtract';
+
+test('subtract', () => {
+  expect(subtract(3, 2)).toBe(1);
+});
