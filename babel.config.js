42403d96544532f751fd5a8d839f503063a43036
Yangshun <tay.yang.shun@gmail.com>
2021-09-20 00:04:29 +0800

    misc: upgrade dependencies

---
+++ babel.config.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current',
+        },
+      },
+    ],
+    '@babel/preset-react',
+    '@babel/preset-flow',
+  ],
+};
--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 // @flow strict-local
 
-console.log("Hello world!");
+const add = (a: number, b: number): number => a + b;
+const subtract = (a: number, b: number): number => a - b;
+
+export {
+  add,
+  subtract
+};
